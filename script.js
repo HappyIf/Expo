a26c0d910624ef590170b502f9cafa4e3501072e
HappyIf <91533349+HappyIf@users.noreply.github.com>
2023-09-22 01:50:49 +0530

    test

---
+++ script.js
@@ -0,0 +1,14 @@
+model = await tf.loadModel('HappyIf/Expo/TB/model/model.json');
+function showFile(input) {
+    let file = input.files[0];
+  
+    let reader = new FileReader();
+
+	let tensor = tf.fromPixels(file)
+	.resizeNearestNeighbor([96,96]) // change the image size here
+	.toFloat()
+	.div(tf.scalar(255.0))
+	.expandDims();
+
+    let predictions = await model.predict(tensor).data();
+  }
